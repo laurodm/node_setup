322199e0972a6607d14a6bbcab6fd1347b0fcf91
laurodm <laurodamasceno@gmail.com>
2021-05-12 22:20:58 -0300

    added typescript and paths

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { request } from 'express'
+import { teste } from '@/controllers/teste'
 
 const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World !' })
+
+    return response.json({ message: 'Hello World!' })
 })
 
 app.listen(3333)
